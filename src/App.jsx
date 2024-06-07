0d75a6e995a85d14e247b15450b972e42656a83f
FSusman <r.usman.tanveer@gmail.com>
2024-06-07 22:21:54 +0500

    Initial creation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-3xl text-center">
+        Bare Minimum setup complete
+      </h1>
+    </>
+  );
+}
+
+export default App;
